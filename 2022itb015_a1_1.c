33e6eecf3d1023e6a16bcb225d0d926948b22748
Kundunilanjan <141931877+Kundunilanjan@users.noreply.github.com>
2023-08-11 16:05:06 +0530

    Create 2022itb015_a1_1.c

---
+++ 2022itb015_a1_1.c
@@ -0,0 +1,11 @@
+3.Write a C program to add and subtract two integers having at least 10 digits each.
+                                 code
+#include<stdio. h>
+ void main(){
+ printsg int
+ long nl, n2;
+ "enter the first number: ") ;
+ Scanf ( "%lld", &nl) ;
+ printf ("'enter the second number:"); Scanf ( "%lld" , &n2) ;
+ printf ("the sum of the digits is %1ld" ,nl+n2); orintf ("\nthe difference of the digits is %lld" , nl-n2) ;
+}
